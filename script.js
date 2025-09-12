b9590ff720a485155c2adef7abac057626ccfa33
Thinh Truong <thinhnguyentruongg@gmail.com>
2025-09-13 01:20:02 +0700

    1

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 600;
+        let offset = sec.offsetTop - 300;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
